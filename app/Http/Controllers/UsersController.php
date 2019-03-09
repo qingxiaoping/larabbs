c924690e031c2716581bc708ee31a9030e50bdb7
vagrant <vagrant@homestead>
2019-03-09 13:59:50 +0000

    xi

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -28,6 +28,7 @@ class UsersController extends Controller
                 $data['avatar'] = $result['path'];
             }
         }
+
         $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
